8f0cbc5f1a1991bd48a943323205ba70aa019450
sakilshikdar <sakilshikdar2504@gmail.com>
2022-04-18 17:33:21 +0600

    not-found-page-done

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -5,7 +5,6 @@ const NotFound = () => {
     return (
         <div>
             <h1 className='text-primary mt-3 text-center'>404 Not Found</h1>
-            {/* <img className='d-block w-300 rounded-3 mx-auto img-fluid ' src={} alt="" /> */}
         </div>
     );
 };
